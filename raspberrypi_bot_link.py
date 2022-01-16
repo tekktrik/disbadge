0a27b3e9bc1f009416a6e472283548104ab9ecc1
tekktrik <89490472+tekktrik@users.noreply.github.com>
2022-01-16 15:42:47 -0500

    And hype command, swap around command names

--- raspberrypi_bot_link.py
+++ raspberrypi_bot_link.py
@@ -11,15 +11,21 @@ async def on_ready():
     print(f"We have logged in as {bot.user}")
 
 @bot.slash_command(guild_ids=[secrets["guild-id"]])
-async def ping(ctx: ApplicationContext, message: str):
-    """Pings the gamer with a message"""
+async def cheer(ctx: ApplicationContext, message: str):
+    """Sends the gamer a message"""
     # TODO: send message to PyBadge
     await ctx.respond("Sending your message to {0}!".format(MY_NAME))
 
 @bot.slash_command(guild_ids=[secrets["guild-id"]])
-async def cheer(ctx: ApplicationContext, message: str):
+async def hype(ctx: ApplicationContext, message: str):
     """Cheers on the gamer with an exciting message!"""
     # TODO: send message to PyBadge
     await ctx.respond("Sending your hype to {0}!".format(MY_NAME))
+
+@bot.slash_command(guild_ids=[secrets["guild-id"]])
+async def ping(ctx: ApplicationContext, message: str):
+    """Pings the gamer with the given message"""
+    # TODO: send message to PyBadge
+    await ctx.respond("Pinging {0} with your message!".format(MY_NAME))
 
 bot.run(secrets["login-token"])
