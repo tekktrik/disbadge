46f17251b72eb9735546cb5d532e7946d6d27ffc
Tekktrik <tekktrik@gmail.com>
2022-01-16 18:32:44 -0500

    Begin adding Bluetooth

--- raspberrypi_bot_link.py
+++ raspberrypi_bot_link.py
@@ -1,9 +1,22 @@
 import discord
 from discord.commands.context import ApplicationContext
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
 from secrets import secrets
 
+# Initialize Bluetooth-related objects
+ble = BLERadio()
+uart = UARTService()
+advertisement = ProvideServicesAdvertisement(uart)
+
+# Set up Bluetooth connection
+ble.start_advertising(advertisement)
+
+# Define variables used throughout Discord bot
 MY_NAME = "Tekktrik"
 
+# Prepare Discord bot
 bot = discord.Bot()
 
 @bot.event
@@ -28,4 +41,5 @@ async def ping(ctx: ApplicationContext, message: str):
     # TODO: send message to PyBadge
     await ctx.respond("Pinging {0} with your message!".format(MY_NAME))
 
+# Run blocking event code
 bot.run(secrets["login-token"])
