2abe37a75c3a245eae814853281fb4358524c625
Alec Delaney <tekktrik@gmail.com>
2022-01-17 22:02:38 -0500

    Prototype of discord_display.py

---
+++ pybadge/discord_display.py
@@ -0,0 +1,12 @@
+import displayio
+from adafruit_display_text.label import Label
+from adafruit_bitmap_font import bitmap_font
+
+# Load the message font
+MESSAGE_FONTNAME = "/fonts/Tewi-11.bdf"
+MESSAGE_FONT = bitmap_font.load_font(MESSAGE_FONTNAME)
+
+class DiscordMessage(displayio.Group):
+
+    def __init__(self, username: str, message: str) -> None:
+        username_label = Label(MESSAGE_FONT, text=username)
