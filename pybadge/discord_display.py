7812835bf8ecaa70ce03a74abc26dbcfa0952d07
tekktrik <89490472+tekktrik@users.noreply.github.com>
2022-01-20 08:59:56 -0500

    Finish DiscordMessage.__init__()

--- pybadge/discord_display.py
+++ pybadge/discord_display.py
@@ -8,5 +8,19 @@ MESSAGE_FONT = bitmap_font.load_font(MESSAGE_FONTNAME)
 
 class DiscordMessage(displayio.Group):
 
-    def __init__(self, username: str, message: str) -> None:
-        username_label = Label(MESSAGE_FONT, text=username)
+    def __init__(self, username: str, message: str, dark_mode: bool = True) -> None:
+
+        super().__init__()
+
+        text_color = 0xFFFFFF if dark_mode else 0x000000
+        self.username_label = Label(MESSAGE_FONT, text=username, color=text_color, y=8)
+        self.append(self.username_label)
+
+        message_lines = DiscordMessage._wrap_nicely(message, 26)
+        max_lines = 4
+        self.wrapped_message = "\n".join(message_lines[:max_lines])
+
+        self.message = Label(MESSAGE_FONT, text=self.wrapped_message, color=text_color, y=32)
+        self.append(self.message)
+
+    # cribbed from adafruit_display_notification
