7336894a534a9470ddaefd74205339614e8afd24
Alec Delaney <tekktrik@gmail.com>
2022-01-26 23:36:18 -0500

    Add neopixel animation setting based on StateIDs

--- pybadge/io.py
+++ pybadge/io.py
@@ -6,6 +6,12 @@ from adafruit_led_animation.animation.pulse import Pulse
 from adafruit_led_animation.color import RED
 from adafruit_led_animation.animation.rainbow import Rainbow
 from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
+from states import StateIDs
+
+try:
+    from typing import Optional
+except ImportError:
+    pass
 
 
 class IOManager:
@@ -29,10 +35,14 @@ class IOManager:
             brightness=0.2,
             auto_write=False)
 
-        self._ping_animation = Pulse(self._neopixels, speed=0.5, color=RED, period=2)
-        self._cheer_animation = Rainbow(self._neopixels, speed=0.1, period=0.75)
-        self._hype_animation = RainbowSparkle(self._neopixels, speed=0.1, period=0.75)
         self._current_animation = None
+        self._animations = {
+            StateIDs.PING: Pulse(self._neopixels, speed=0.5, color=RED, period=2),
+            StateIDs.CHEER: Rainbow(self._neopixels, speed=0.1, period=0.75),
+            StateIDs.HYPE: RainbowSparkle(self._neopixels, speed=0.1, period=0.75),
+            StateIDs.MESSAGE: None,
+            StateIDs.NO_MESSAGE: None,
+        }
 
     def update_inputs(self) -> bool:
         self._pad.events.get_into(self._event)
@@ -44,3 +54,11 @@ class IOManager:
     @property
     def button_pressed(self) -> int:
         return self._event.key_number
+
+    @property
+    def animation(self) -> Optional[int]:
+        return self._current_animation
+
+    @animation.setter
+    def animation(self, id: int) -> None:
+        self._current_animation = self._animations.get(id)
