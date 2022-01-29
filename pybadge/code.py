397d7874249b3ee63c8d51dea26bf0ae35a0e60f
Alec Delaney <tekktrik@gmail.com>
2022-01-29 10:03:17 -0500

    Linted and reformatted pybadge/code.py

--- pybadge/code.py
+++ pybadge/code.py
@@ -3,24 +3,16 @@ This is a Conference Badge type Name Tag that is intended to be displayed on
 the PyBadge. Feel free to customize it to your heart's content.
 """
 
-import time
-import board
-from micropython import const
 import asyncio
+import board
 from adafruit_airlift.esp32 import ESP32
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 from pybadge_messages import DiscordMessageGroup
 from disbadge import DiscordPyBadge
-from shared.messages import CommandType
+from states import DisplayStateIDs
 from shared.uart import UARTManager
-from states import DisplayStateIDs, LEDStateIDs
-
-try:
-    from typing import Optional
-except ImportError:
-    pass
 
 disbadge = DiscordPyBadge(external_speaker=True)
 disbadge.set_splash(DisplayStateIDs.LOADING)
@@ -41,6 +33,10 @@ CURRENT_MESSAGE = DiscordMessageGroup()
 
 
 async def data_transmission(uart: UARTService):
+    """Sub-sequence for handling data transmission over BLE UART
+
+    :param UARTService uart: The UARTService connection object
+    """
 
     with UARTManager(uart, ble) as uart_manager:
         while uart_manager.connected:
@@ -51,6 +47,7 @@ async def data_transmission(uart: UARTService):
 
 
 async def ui_management():
+    """Sub-sequence for handling the user input and interface"""
 
     while ble.connected:
 
@@ -59,30 +56,35 @@ async def ui_management():
         asyncio.sleep(0)
 
 
-# Main loop
-while True:
+def main():
+    """Main sequence"""
 
-    # If connected, look through connections and connect to one with UARTService
-    if ble.connected and any(
-        UARTService in connection for connection in ble.connections
-    ):
-        print("UARTService found in connection, getting connection...")
-        for connection in ble.connections:
-            if UARTService not in connection:
+    while True:
+
+        # If connected, look through connections and connect to one with UARTService
+        if ble.connected and any(
+            UARTService in connection for connection in ble.connections
+        ):
+            print("UARTService found in connection, getting connection...")
+            for connection in ble.connections:
+                if UARTService not in connection:
+                    continue
+                uart: UARTService = connection[UARTService]
+                print("UARTService connected!")
+
+                # Create and await tasks to achieve main functionality at this level
+                data_task = asyncio.create_task(data_transmission(uart))
+                ui_task = asyncio.create_task(ui_management())
+                asyncio.gather(data_task, ui_task)
+
+        # If not connected, attempt to do so
+        disbadge.set_splash(DisplayStateIDs.CONNECTING)
+        for advertisement in ble.start_scan(ProvideServicesAdvertisement, timeout=1):
+            advertisement: ProvideServicesAdvertisement
+            if UARTService not in advertisement.services:
                 continue
-            uart: UARTService = connection[UARTService]
-            print("UARTService connected!")
+            ble.connect(advertisement)
+            print("Connected!")
 
-            # Create and await tasks to achieve main functionality at this level
-            data_task = asyncio.create_task(data_transmission(uart))
-            ui_task = asyncio.create_task(ui_management())
-            asyncio.gather(data_task, ui_task)
 
-    # If not connected, attempt to do so
-    disbadge.set_splash(DisplayStateIDs.CONNECTING)
-    for advertisement in ble.start_scan(ProvideServicesAdvertisement, timeout=1):
-        advertisement: ProvideServicesAdvertisement
-        if UARTService not in advertisement.services:
-            continue
-        ble.connect(advertisement)
-        print("Connected!")
+main()
