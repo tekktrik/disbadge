d1f9e3d3bf71179688b0b4ac1e18fc3ed5303cde
Alec Delaney <tekktrik@gmail.com>
2022-01-29 11:28:10 -0500

    Add sub-sequence for loading and displaying messages

--- pybadge/code.py
+++ pybadge/code.py
@@ -9,15 +9,22 @@ Main sequence for Discord PyBadge
 """
 
 import asyncio
+import gc
+import time
 import board
 from adafruit_airlift.esp32 import ESP32
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
+from pybadge.states import LEDStateIDs
 from pybadge_messages import DiscordMessageGroup
-from disbadge import DiscordPyBadge
+from disbadge import DiscordPyBadge, Buttons
+from shared.messages import CommandType
 from states import DisplayStateIDs
 from shared.uart import UARTManager
+
+MESSAGE_PIN_TIME = 10
+"""How long messages should be displayed before being removed"""
 
 disbadge = DiscordPyBadge(external_speaker=True)
 disbadge.set_splash(DisplayStateIDs.LOADING)
@@ -47,7 +54,9 @@ async def data_transmission(uart: UARTService):
         while uart_manager.connected:
             while uart_manager.data_available:
                 message_dict = uart_manager.receive()
+                CURRENT_MESSAGE = DiscordMessageGroup()
                 CURRENT_MESSAGE.from_dict(message_dict)
+                # gc.collect()
             asyncio.sleep(0)
 
 
@@ -57,6 +66,39 @@ async def ui_management():
     while ble.connected:
 
         # Main loop for handling UI and buttons
+        if CURRENT_MESSAGE != disbadge.current_message:
+            
+            if CURRENT_MESSAGE is None:
+                disbadge.set_splash(DisplayStateIDs.NO_MESSAGE)
+            else:
+                CURRENT_MESSAGE: DiscordMessageGroup
+                if CURRENT_MESSAGE.cmd_type == CommandType.PING:
+                    led_animation_id = LEDStateIDs.PING
+                    new_splash_id = DisplayStateIDs.PING
+                elif CURRENT_MESSAGE.cmd_type == CommandType.CHEER:
+                    led_animation_id = LEDStateIDs.CHEER
+                    new_splash_id = DisplayStateIDs.CHEER
+                else:
+                    led_animation_id = LEDStateIDs.HYPE
+                    new_splash_id = DisplayStateIDs.HYPE
+                disbadge.set_splash(new_splash_id)
+                disbadge.animation = led_animation_id
+                disbadge.play_notification(new_splash_id)
+                disbadge.set_splash(DisplayStateIDs.MESSAGE, message=CURRENT_MESSAGE)
+
+                popup_start_time = time.monotonic()
+                while time.monotonic() < popup_start_time + (MESSAGE_PIN_TIME*60) or CURRENT_MESSAGE != disbadge.current_message:
+                    disbadge.animate_leds()
+                    if disbadge.button_pressed == Buttons.BUTTON_B:
+                        break
+                    asyncio.sleep(0)
+                disbadge.animation = LEDStateIDs.NONE
+                if CURRENT_MESSAGE != disbadge.current_message: # New message
+                    disbadge.set_splash(DisplayStateIDs.LOADING)
+                    continue
+                else: # Timed out
+                    disbadge.set_splash(DisplayStateIDs.NO_MESSAGE)
+                    CURRENT_MESSAGE = None
 
         asyncio.sleep(0)
 
