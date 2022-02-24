5fef4c338db0d31f8eb8afe6efe6d30540bd0117
Alec Delaney <tekktrik@gmail.com>
2022-02-24 12:43:38 -0500

    Add type annotations

--- shared/uri_codec.py
+++ shared/uri_codec.py
@@ -5,17 +5,17 @@ import gc
 
 IS_HOST = 'typing' in sys.modules
 
-def _is_alphanumeric(character: str):
+def _is_alphanumeric(character: str) -> bool:
     return re.match("^[a-zA-Z0-9]+$", character)
 
-def _encode_character(character: str):
+def _encode_character(character: str) -> str:
 
     if not isinstance(character, str):
         raise TypeError("Can only encode a string character")
 
     return "".join(["-", str(ord(character)), "-"])
 
-def _decode_character(char_str: str):
+def _decode_character(char_str: str) -> str:
 
     if not isinstance(char_str, str):
         raise TypeError("Can only decode a string representation of a character")
