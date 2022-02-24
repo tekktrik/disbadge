0c7ac28153275ac37bebd82c7064d4e0c58af5fb
Alec Delaney <tekktrik@gmail.com>
2022-02-24 12:41:55 -0500

    Finish prototype for message codec

--- shared/uri_codec.py
+++ shared/uri_codec.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import gc
 
 try:
     import typing  # pylint: disable=unused-import
@@ -13,16 +14,44 @@ def _is_alphanumeric(character: str):
     return re.match("^[a-zA-Z0-9]+$", character)
 
 def _encode_character(character: str):
+
+    if not isinstance(character, str):
+        raise TypeError("Can only encode a string character")
+
     return "".join(["-", str(ord(character)), "-"])
 
-def _decode_character():
-    # TODO: Add details
-    pass
+def _decode_character(char_str: str):
 
-def encode(payload: str):
-    new_payload = ""
-    for char in payload:
+    if not isinstance(char_str, str):
+        raise TypeError("Can only decode a string representation of a character")
+    
+    if not char_str.startswith("-") or not char_str.endswith("-"):
+        raise ValueError("Not a decodable character string")
+
+    return chr(int(char_str[1:-1]))
+
+def encode(original: str) -> str:
+    payload = ""
+    for char in original:
         if not _is_alphanumeric(char):
             char = _encode_character(char)
-        new_payload += char
+        payload += char
+    gc.collect()
+    return payload
 
+def decode(payload: str) -> str:
+    original = ""
+    payload_iter = iter(payload)
+    for char in payload_iter:
+        if char == "-":
+            sub_seq = char
+            try:
+                while True:
+                    next_char = next(payload)
+                    sub_seq += next_char
+                    if next_char == "-":
+                        break
+            except StopIteration:
+                raise RuntimeError("Could not parse a special character in string")
+            char = _decode_character(sub_seq)
+        original += char
