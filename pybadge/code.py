486330cec857062adb39085dc86d118a7ffa613e
Alec Delaney <tekktrik@gmail.com>
2022-02-25 13:40:07 -0500

    Remove button press sequence in startup

--- pybadge/code.py
+++ pybadge/code.py
@@ -77,11 +77,6 @@ pretty_ip_address = esp32.pretty_ip(esp32.ip_address)
 disbadge.ip_address = pretty_ip_address
 print(pretty_ip_address)
 disbadge.set_splash(DisplayStateIDs.CONNECT)
-while True:
-    disbadge.update_inputs()
-    if disbadge.button_pressed == Buttons.BUTTON_A:
-        disbadge.set_splash(DisplayStateIDs.WAITING)
-        break
 while not global_state.DISCORD_CONNECTION:
     wsgi_server.update_poll()
 disbadge.set_splash(DisplayStateIDs.NO_MESSAGE)
